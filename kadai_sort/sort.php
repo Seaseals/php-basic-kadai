b2add41efe7a3ccf092bc65ce89cde9d38455501
Seaseals <seaiayumi27@gmail.com>
2025-07-25 13:22:07 +0900

    コメントを分かりやすく追加しました

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -10,34 +10,46 @@
     <p>
         <?php
 
+        //配列を定義する 
          $nums = array(15, 4, 18, 23, 10 );
 
+        //  昇順・降順を選択して配列をソートする関数
+        //  ( )内の引数には関数仕様のとおり、
+        // $array（ソート対象の配列）と$order（ソート方向）を指定しなければなりません。
         function sort_2way($nums,$order){
         
-        if($order=== true){
+        // TRUEなら昇順、TRUEでなければ降順にソートする
         
-        // sort_2way($nums ,TRUE);
+        // 引数$orderの値をチェックし、TRUEなら昇順、TRUEでないなら降準でソートする。
+        if($order){
+        // if($order=== true){でもOK。
         
         echo "昇順にソートします。<br>";
-
         sort($nums); 
+
+        // foreach文を用いてソート結果を1行ずつ表示する処理
+        // $numsからデータを$keyとして1つずつ取り出し、
+        // echo文で順番に出力しています。
         foreach($nums as $key => $val){
         echo "{$val} <br>";
         }
 
         }else{
-          
-          // sort_2way($nums,FALSE);      
-          
+                    
           echo "降順にソートします。<br>";
           rsort($nums);
-      foreach ($nums as $key => $val) {
+          // foreach文を用いてソート結果を1行ずつ表示する処理
+          // $numsからデータを$keyとして1つずつ取り出し、
+          // echo文で順番に出力しています。
+          foreach ($nums as $key => $val) {
           echo "{$val} <br>";
         }
       }
     }
 
+    // 昇順にソート
       sort_2way($nums ,TRUE);
+    // 降順にソート
       sort_2way($nums ,FALSE);
     
         
